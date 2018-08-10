2f46eea88d758950481924d3d2284ee0863a47a8
agorski <adrian.gorski@novaris.pl>
2018-08-10 09:52:07 +0200

    removed @Param from Repositories + removed imports

--- src/main/java/pl/drinkbook/repository/IDrinkRepository.java
+++ src/main/java/pl/drinkbook/repository/IDrinkRepository.java
@@ -2,7 +2,6 @@ package pl.drinkbook.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import pl.drinkbook.entities.DrinkBo;
 
 public interface IDrinkRepository extends JpaRepository<DrinkBo, Long> {
